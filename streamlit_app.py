69bf11d8cff7e73038855e159613d2f123afd7ea
surya-prakash-patel <134393386+surya-prakash-patel@users.noreply.github.com>
2023-05-23 18:28:10 +0530

    Update streamlit_app.py
    added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled free-Range egg')
+streamlit.header(' Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled free-Range egg')
+streamlit.text('🥑 Avocado Toast')
